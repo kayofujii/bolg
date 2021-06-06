5f32b5c1d6dcee0a943c18318904e4a6d0a62426
kayofujii <kayosamu1025@gmail.com>
2021-06-06 17:27:26 +0900

    forms and user accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,17 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 from .views import (
-    BlogListView,
-    BlogDetailView,
-    BlogCreateView,
-    BlogUpdateView,
-    BlogDeleteView,
+    SignUpView,
 )
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home'),
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
-    path('post/new/', BlogCreateView.as_view(), name='post_new'),
-    path('post/edit/<int:pk>/', BlogUpdateView.as_view(), name='post_edit'),
-    path('post/delete/<int:pk>/', BlogDeleteView.as_view(), name='post_delete'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
